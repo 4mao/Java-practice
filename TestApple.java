cfbdf632d33615f4fac1d2fd1af144495d92d8cb
4mao <59215007+4mao@users.noreply.github.com>
2019-12-25 17:51:20 +0800

    测试toSting重写后的效果
    测试toSting重写后的效果

---
+++ TestApple.java
@@ -0,0 +1,10 @@
+package chapter6;
+
+public class TestApple {
+   public static void main(String[] args) {
+	Apple apple = new Apple();
+	apple.setColor("红色");
+	apple.setWeigth(56);
+	System.out.println(apple);
+}
+}
